d1f73e18fa53155280c39a357dcd075536b5d902
Christopher Givens <jordanwhitneymartin@gmail.com>
2018-02-10 15:58:41 -0600

    python

--- py/hashpdf.py
+++ py/hashpdf.py
@@ -1,3 +1,17 @@
 import pdfkit
 
-pdfkit.from_url("http://hashtagblessed.github.io/", "out.pdf",)
+
+
+options = {
+    'page-size': 'Letter',
+    'encoding': "UTF-8",
+    'javascript-delay': 1000,
+  
+
+
+}
+
+
+pdfkit.from_file('/Users/chrisgivens/hashtagblessed.github.io/index.html', 'out.pdf', options=options)
+
+#pdfkit.from_url('https://hashtagblessed.github.io/', 'out.pdf', options=options)
