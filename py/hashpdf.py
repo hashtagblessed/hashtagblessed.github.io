022d76956310135129d7217cde3a3d4a7c58135d
Christopher Givens <jordanwhitneymartin@gmail.com>
2018-02-10 21:14:09 -0600

    everything working sort of

--- py/hashpdf.py
+++ py/hashpdf.py
@@ -1,17 +1,21 @@
 import pdfkit
-
+import os
 
 
 options = {
     'page-size': 'Letter',
-    'encoding': "UTF-8",
-    'javascript-delay': 1000,
-  
-
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    #'javascript-delay': 100,
+  	'encoding': 'UTF-8',
 
 }
 
 
 pdfkit.from_file('/Users/chrisgivens/hashtagblessed.github.io/index.html', 'out.pdf', options=options)
+
+os.startfile("YourDocument", "print")
 
 #pdfkit.from_url('https://hashtagblessed.github.io/', 'out.pdf', options=options)
